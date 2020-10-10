7312040c07f9b57d9b9c57637399dc77916c8830
rick6502 <rick6502@users.noreply.github.com>
2020-10-09 17:33:24 -0700

    DAC setup
    Initial setup of the connection string, config read  and basic DAC

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -6,21 +6,26 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using BootStrapMVCDev.Models;
+using Microsoft.Extensions.Configuration;
+
 
 namespace BootStrapMVCDev.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private IConfiguration Configuration;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IConfiguration _configuration)
         {
             _logger = logger;
+            Configuration = _configuration;
         }
 
-        public IActionResult Index()
+        public String Index()
         {
-            return View();
+            string connString = this.Configuration.GetConnectionString("DefaultConnection");
+            return connString;
         }
 
         public IActionResult Privacy()
---
+++ DAC/StaffTableDAC.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+using System.Data.SqlClient;
+
+namespace BootStrapMVCDev.DAC
+{
+    public class StaffTableDAC
+    {
+        public static string ConnectionString { get; set; }
+
+        public StaffTableDAC(string connectString)
+        {
+            StaffTableDAC.ConnectionString = connectString;
+        }
+
+        private static SqlConnection DBConnectionSQL
+        {
+            get
+            {
+                SqlConnection sqlConn = new SqlConnection();
+                string connString = ConnectionString;
+                sqlConn.ConnectionString = connString;
+                return sqlConn;
+            }
+        }
+
+
+    }
+}
